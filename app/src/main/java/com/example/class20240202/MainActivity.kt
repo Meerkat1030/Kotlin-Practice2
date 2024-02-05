09687992afe17d5f3d6f000c3e3a8ddb3e84330f
Meerkat1030 <hong991000@gmail.com>
2024-02-05 09:46:13 +0900

    [COMMIT] 02/05 commit 다양한 알림 연습

--- app/src/main/java/com/example/class20240202/MainActivity.kt
+++ app/src/main/java/com/example/class20240202/MainActivity.kt
@@ -2,14 +2,14 @@ package com.example.class20240202
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
-import android.app.Dialog
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.RemoteInput
 import android.app.TimePickerDialog
 import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.media.AudioAttributes
 import android.media.RingtoneManager
@@ -22,32 +22,36 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.TimePicker
 import androidx.core.app.NotificationCompat
+import androidx.core.app.Person
+import androidx.core.app.RemoteInput
+import androidx.core.graphics.drawable.IconCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        //날짜 선택
-//        DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener{
-//            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-//                Log.d("test", "$year 년 ${month + 1}, dayOfMonth : $dayOfMonth")
-//            }
-//        }, 2024, 1, 2).show()
-//
-//        //시간 선택
-//        TimePickerDialog(this, object: TimePickerDialog.OnTimeSetListener{
+        // 날짜 선택
+//         DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener {
+//             override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+//                 Log.d("test", "${year}년 ${month + 1}월 ${dayOfMonth}일")
+//             }
+//         }, 2024, 1, 2).show()
+
+        // 시간 선택
+//        TimePickerDialog(this, object: TimePickerDialog.OnTimeSetListener {
 //            override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
 //                Log.d("test", "$hourOfDay : $minute")
 //            }
 //        }, 15, 0, true).show()
+
         // 체크박스를 포함하는 알림창
         val items = arrayOf<String>("사과", "복숭아", "수박", "딸기")
-        AlertDialog.Builder(this).run{
+        AlertDialog.Builder(this).run {
             setTitle("과일 리스트")
             setIcon(android.R.drawable.ic_dialog_info)
             setMultiChoiceItems(items, booleanArrayOf(true, false, true, false),
-                object:DialogInterface.OnMultiChoiceClickListener{
+                object:DialogInterface.OnMultiChoiceClickListener {
                     override fun onClick(dialog: DialogInterface?, which: Int, isChecked: Boolean) {
                         Log.d("test", "${items[which]} 이 " +
                                 "${if(isChecked) "선택되었습니다." else "선택 해제되었습니다."}")
@@ -64,7 +68,7 @@ class MainActivity : AppCompatActivity() {
         val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         val builder: NotificationCompat.Builder
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channelId = "one-channel"
             val channelName = "My Channel One"
             val channel = NotificationChannel(
@@ -85,14 +89,14 @@ class MainActivity : AppCompatActivity() {
             channel.enableLights(true)
             channel.lightColor = Color.RED
             channel.enableVibration(true)
-            channel.vibrationPattern = longArrayOf(100,200,100,200)
+            channel.vibrationPattern = longArrayOf(100, 200, 100, 200)
 
             // 채널을 NotificationManager에 등록
             manager.createNotificationChannel(channel)
 
             // 채널을 이용해서 빌더 생성
             builder = NotificationCompat.Builder(this, channelId)
-        }else{
+        } else {
             builder = NotificationCompat.Builder(this, "")
         }
 
@@ -102,19 +106,19 @@ class MainActivity : AppCompatActivity() {
         builder.setContentTitle("알림 제목")
         builder.setContentText("알림 내용입니다.")
 
-        // 알림 취소 막기
+        // 알림 취소 막기(동작 안함)
         // 알림 터치해도 알림 사라지지 않음
-        builder.setAutoCancel(false)
+        //builder.setAutoCancel(false)
         // 알림 스와이프해도 사라지지 않음
-        builder.setOngoing(true)
+        //builder.setOngoing(true)
 
         // 액션 등록
-        val actionIntent = Intent(this, ReplyReceiver::class.java)
-        val replyPendingIntent = PendingIntent.getBroadcast(this, 20, actionIntent,
-            PendingIntent.FLAG_IMMUTABLE)
+        val replyIntent = Intent(this, ReplyReceiver::class.java)
+        val replyPendingIntent = PendingIntent.getBroadcast(this, 30, replyIntent,
+            PendingIntent.FLAG_MUTABLE)
         val KEY_TEXT_REPLY = "key_text_reply"
         var replyLabel: String = "답장"
-        var remoteInput: RemoteInput = RemoteInput.Builder(KEY_TEXT_REPLY).run{
+        var remoteInput: RemoteInput = RemoteInput.Builder(KEY_TEXT_REPLY).run {
             setLabel(replyLabel)
             build()
         }
@@ -124,12 +128,62 @@ class MainActivity : AppCompatActivity() {
             ).addRemoteInput(remoteInput).build()
         )
 
+        // 프로그래스바
+//        builder.setProgress(100,0,true)
+
+        // 알림 스타일
+        // 큰 이미지 스타일(BigPictureStyle)
+        val bigPicture = BitmapFactory.decodeResource(resources, R.drawable.dog1)
+        val bigStyle = NotificationCompat.BigPictureStyle()
+        bigStyle.bigPicture(bigPicture)
+//        builder.setStyle(bigStyle)
+
+        // 긴 텍스트 스타일(BigTextStyle)
+        val bigTextStyle = NotificationCompat.BigTextStyle()
+        bigTextStyle.bigText(resources.getString(R.string.long_text))
+//        builder.setStyle(bigTextStyle)
+
+        // 상자 스타일(InboxStyle)
+        val style = NotificationCompat.InboxStyle()
+        style.addLine("1코스 - 수락.불암산코스")
+        style.addLine("2코스 - 용마.아차산코스")
+        style.addLine("3코스 - 고덕.일자산코스")
+        style.addLine("4코스 - 대모.우면산코스")
+//        builder.setStyle(style)
+
+        // 메시지 스타일(Message)
+        val sender1: Person = Person.Builder()
+            .setName("Lee JaeHong")
+            .build()
+
+        val sender2: Person = Person.Builder()
+            .setName("MeerKat")
+            .build()
+
+        val message1 = NotificationCompat.MessagingStyle.Message(
+            "hello",
+            System.currentTimeMillis(),
+            sender1
+        )
+
+        val message2 = NotificationCompat.MessagingStyle.Message(
+            "world",
+            System.currentTimeMillis(),
+            sender2
+        )
+
+        val messageStyle = NotificationCompat.MessagingStyle(sender1)
+            .addMessage(message1)
+            .addMessage(message2)
+        builder.setStyle(messageStyle)
+
         // 알림 발생
         manager.notify(11, builder.build())
 
+        // 알림 끄기 버튼 클릭시
         val btnCancel = findViewById<Button>(R.id.btnCancel)
-        btnCancel.setOnClickListener{
-
+        btnCancel.setOnClickListener {
+            // 알림 제거
             manager.cancel(11)
         }
     }
